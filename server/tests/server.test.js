8278d180a3e47a12f3800a87c51885a573d3b79d
Joe Grotto <joegrotto@heir.company>
2017-03-28 17:09:13 -0500

    testing for login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -222,7 +222,7 @@ describe(`GET /todos/:id`, () => {
                          expect(user).toExist();
                          expect(user.password).toNotBe(password);
                          done();
-                    });
+                    }).catch((e) => done(e));
                });
           });
 
@@ -236,7 +236,7 @@ describe(`GET /todos/:id`, () => {
                .expect(400)
                .end(done);
           });
-          
+
           it(`should notify user if email is in use`, (done) => {
                request(app)
                .post(`/users`)
@@ -246,5 +246,54 @@ describe(`GET /todos/:id`, () => {
                })
                .expect(400)
                .end(done);
+          });
+     });
+
+     describe(`POST /users/login`, () => {
+          it(`should validate user credentials, login and return auth token`, (done) => {
+               request(app)
+                    .post(`/users/login`)
+                    .send({
+                         email: users[1].email,
+                         password: users[1].password
+                    })
+                    .expect(200)
+                    .expect((res) => {
+                         expect(res.headers[`x-auth`]).toExist();
+                    })
+                    .end((err, res) => {
+                         if (err) {
+                              return done(err);
+                         }
+                         User.findById(users[1]._id).then((user) => {
+                              expect(user.tokens[0]).toInclude({
+                                   access: `auth`,
+                                   token: res.headers[`x-auth`]
+                              });
+                              done();
+                         }).catch((e) => done(e));
+                    });
+          });
+
+          it(`should reject invalid login`, (done) => {
+               request(app)
+                    .post(`/users/login`)
+                    .send({
+                         email: users[1].email,
+                         password: users[1].password +`1`
+                    })
+                    .expect(400)
+                    .expect((res) => {
+                         expect(res.headers[`x-auth`]).toNotExist();
+                    })
+                    .end((err, res) => {
+                         if (err) {
+                              return done(err);
+                         }
+                         User.findById(users[1]._id).then((user) => {
+                              expect(user.tokens.length).toBe(0);
+                              done();
+                         }).catch((e) => done(e));
+                    });
           });
      });
