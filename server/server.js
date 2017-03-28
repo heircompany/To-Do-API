93b1114372a008eaa4d119b81d30d8d983f274ea
Joe Grotto <joegrotto@heir.company>
2017-03-28 11:37:53 -0500

    added PATCH route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
+const _ = require(`lodash`);
 const {ObjectID} = require(`mongodb`);
 const {mongoose} = require(`./db/mongoose`);
 const {Todo} = require(`./models/todo`);
@@ -86,6 +87,33 @@ app.delete(`/todos/:id`, (req, res) => {
     }).catch((e) => {
       res.status(400).send();
     });
+});
+
+app.patch(`/todos/:id`, (req, res) => {
+     let id = req.params.id;
+     // pick values user can edit
+     let body = _.pick(req.body, [`text`, `completed`]);
+
+     if (!ObjectID.isValid(id)) {
+          return res.status(404).send();
+     }
+     //check if values are Boolean and get Date/Time stamps
+     if (_.isBoolean(body.completed) && body.completed) {
+          body.completedAt = new Date().getTime();
+     } else {
+          // set completed to false
+          body.completed = false;
+          //set completed at to blank value
+          body.completedAt = null;
+     }
+     Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+          if (!todo) {
+               return res.status(404).send();
+          }
+          res.send({todo});
+     }).catch((e) => {
+          res.status(400).send();
+     });
 });
 
 app.listen(port, () => {
